53fca1e4597bbea29f316ce6f6f00fa1509647e8
ExcessMarrow773 <139408126+ExcessMarrow773@users.noreply.github.com>
2024-07-27 10:23:36 -0500

    Add files via upload

---
+++ client.py
@@ -0,0 +1,55 @@
+from os import environ
+environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
+import socket, threading, pygame, sys
+
+f = open(r"custom chat\name.txt", "r")
+username = f.read()
+pygame.mixer.init()
+
+def play_sound(i):
+    if i == 1:
+        pygame.mixer.music.load("custom chat\music\Bad To The Bone Riff Sound Effect.mp3")
+    elif i == 2:
+        pygame.mixer.music.load(r"custom chat\music\taco-bell.mp3")
+    pygame.mixer.music.play()
+def receive_messages():
+    while True:
+        try:
+            message = client_socket.recv(1024).decode('utf-8')
+            if not message:
+                break
+            if message == "bttb.mp3":
+                play_sound(1)
+            elif message == "tbbe.mp3":
+                play_sound(2)
+            print(message)
+        except:
+            break
+
+def send_messages():
+    while True:
+        message = input()
+        if message == "bttb.mp3":
+            message = message
+            play_sound(1)
+        elif message == "tbbe.mp3":
+            play_sound(2)
+        elif message == "{[exit]}":
+            exit()
+        else:
+            message = f"{username} : {message}"
+        client_socket.send(message.encode('utf-8'))
+
+def start_client():
+    global client_socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('127.0.0.1', 12345))
+
+    receive_thread = threading.Thread(target=receive_messages)
+    receive_thread.start()
+
+    send_thread = threading.Thread(target=send_messages)
+    send_thread.start()
+
+if __name__ == "__main__":
+    start_client()
---
+++ server.py
@@ -0,0 +1,44 @@
+import socket
+import threading
+
+# Function to handle client connections
+def handle_client(client_socket, client_address):
+    print(f"[+] New connection from {client_address}")
+    while True:
+        try:
+            message = client_socket.recv(1024).decode('utf-8')
+            if not message:
+                break
+            broadcast_message(message, client_socket)
+        except:
+            clients.remove(client_socket)
+            break
+
+    client_socket.close()
+
+# Function to broadcast messages to all clients
+def broadcast_message(message, sender_socket):
+    for client in clients:
+        if client != sender_socket:
+            try:
+                client.send(message.encode('utf-8'))
+            except:
+                clients.remove(client)
+
+# Main function to start the server
+def start_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('0.0.0.0', 12345))
+    server.listen(5)
+    print("[*] Server listening on port 12345")
+
+    while True:
+        client_socket, client_address = server.accept()
+        clients.append(client_socket)
+        client_handler = threading.Thread(target=handle_client, args=(client_socket, client_address))
+        client_handler.start()
+
+clients = []
+
+if __name__ == "__main__":
+    start_server()
